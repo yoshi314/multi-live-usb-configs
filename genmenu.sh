cca3dbf525c0816a5655747c4641befb93d6e8f7
yoshi314 <yoshi314@gmail.com>
2012-09-18 13:24:10 +0200

    small sanity check

--- genmenu.sh
+++ genmenu.sh
@@ -3,6 +3,11 @@
 # this script will generate a simple menu from available .cfg files
 # header will be taken from syslinux.cfg
 
+if [ ! -f "syslinux.cfg" ] ; then
+	echo "syslinux.cfg file was not found. please copy it over from repository and try again"
+	exit 1
+fi
+
 sed -n '/#header start/,/#header end/p' syslinux.cfg
 
 for plik in $( ls -1 *.cfg  | egrep -v '(syslinux.cfg|isolinux.cfg|extlinux.cfg)' )
